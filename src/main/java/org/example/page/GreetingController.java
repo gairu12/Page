cae81d236dc4409c3d92ce8d0be419b3a5d904fe
Rufina Gairu <sh.gairu@gmail.com>
2020-11-10 19:30:48 +0600

    Initial commit

--- src/main/java/org/example/page/GreetingController.java
+++ src/main/java/org/example/page/GreetingController.java
@@ -16,13 +16,11 @@ public class GreetingController {
 	@Autowired
 	private MessageRepo messageRepo;
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
-						   Map<String, Object> model) {
-		model.put("name", name);
+	@GetMapping("/")
+	public String greeting(Map<String, Object> model) {
 		return "greeting";
 	}
-	@GetMapping
+	@GetMapping("/main")
 	public String main(Map<String, Object> model) {
 		Iterable<Message> messages = messageRepo.findAll();
 		model.put("messages", messages);
@@ -30,7 +28,7 @@ public class GreetingController {
 		return "main";
 	}
 
-	@PostMapping
+	@PostMapping("/main")
 	public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
 		Message message = new Message(text, tag);
 
@@ -44,7 +42,7 @@ public class GreetingController {
 	@PostMapping("filter")
 	public String filter(@RequestParam String filter, Map<String, Object> model) {
 		Iterable<Message> messages;
-		if (filter != null || filter.isEmpty()) {
+		if (filter != null) {
 			messages = messageRepo.findByTag(filter);
 		} else {
 			messages = messageRepo.findAll();
